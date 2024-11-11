dedb094b62cb0fd12358a2eebaba7a12c3972162
lottapispa <lotta.pispa@helsinki.fi>
2024-11-11 23:28:48 +0200

    11 tehtävään saakka

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,17 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
-    
+        self.nationality = dict["nationality"]
+        self.assists = dict["assists"]
+        self.goals = dict["goals"]
+        self.team = dict["team"]
+        self.games = dict["games"]
+        self.id = dict["id"]
+        self.points = dict["assists"]+dict["goals"]
+
     def __str__(self):
-        return self.name
+        return (
+            f"{self.name:20}" 
+            f"{self.team or '-'}  "
+            f"{self.goals:<2} + {self.assists:<2} = {self.points:>1}"
+        )
