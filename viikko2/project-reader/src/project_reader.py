d7dc03983761141a4e9abd5803306bd984789e31
lottapispa <lotta.pispa@helsinki.fi>
2024-11-11 18:16:33 +0200

    laskarit 1 ja 2

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
 from project import Project
-
+import toml
 
 class ProjectReader:
     def __init__(self, url):
@@ -9,7 +9,7 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        parsed_toml = toml.loads(content)
+        return Project(parsed_toml["tool"]["poetry"]["name"], parsed_toml["tool"]["poetry"]["description"], parsed_toml["tool"]["poetry"]["license"], parsed_toml["tool"]["poetry"]["authors"], parsed_toml["tool"]["poetry"]["dependencies"], parsed_toml["tool"]["poetry"]["group"]["dev"]["dependencies"])
