21efec3eb997da5afeae1b7e811eb52ae355706f
lottapispa <lotta.pispa@helsinki.fi>
2024-11-11 21:18:42 +0200

    tulo

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -3,6 +3,7 @@
 from logger import logger
 from summa import summa
 from erotus import erotus
+from tulo import tulo
 
 logger("aloitetaan ohjelma")
 
@@ -11,6 +12,7 @@ y = int(input("luku 2: "))
 
 print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")
 print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")
+print(f"{x} * {y} = {tulo(x, y)}") 
 
 logger("lopetetaan ohjelma")
 print("goodbye!")
