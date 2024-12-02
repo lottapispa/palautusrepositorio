2502f50aba2abd30f2e6fb00062646d8581c1fcf
lottapispa <lotta.pispa@helsinki.fi>
2024-12-02 21:59:13 +0200

    tehtävä 5 ja yritys tehtävään 6

--- viikko5/laskin/src/kayttoliittyma.py
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -8,11 +8,80 @@ class Komento(Enum):
     NOLLAUS = 3
     KUMOA = 4
 
+class Summa():
+    def __init__(self, sovelluslogiikka, syote, edellinen_komento):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._syote = syote
+        self._edellinen_komento = edellinen_komento
+
+    def suorita(self):
+        try:
+            self._sovelluslogiikka.plus(int(self._syote()))
+            self._edellinen_komento = [self._sovelluslogiikka.arvo(), "summa"]
+        except:
+            print("Tyhjä kenttä")
+
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self._edellinen_komento[0])
+
+class Erotus():
+    def __init__(self, sovelluslogiikka, syote, edellinen_komento):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._syote = syote
+        self._edellinen_komento = edellinen_komento
+
+    def suorita(self):
+        try:
+            self._sovelluslogiikka.miinus(int(self._syote()))
+            self._edellinen_komento = [self._sovelluslogiikka.arvo(), "erotus"]
+        except:
+            print("Tyhjä kenttä")
+
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self._edellinen_komento[0])
+
+class Nollaus():
+    def __init__(self, sovelluslogiikka, syote, edellinen_komento):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._syote = syote
+        self._edellinen_komento = edellinen_komento
+
+    def suorita(self):
+        self._sovelluslogiikka.nollaa()
+        self._edellinen_komento = [self._sovelluslogiikka.arvo(), "nollaus"]
+
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self._edellinen_komento[0])
+
+class Kumoa():
+    def __init__(self, sovelluslogiikka, syote, edellinen_komento):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._syote = syote
+        self._edellinen_komento = edellinen_komento
+
+    def suorita(self):
+        if self._edellinen_komento[1] == "summa":
+            self._komennot[Komento.SUMMA].kumoa()
+        elif self._edellinen_komento[1] == "erotus":
+            self._komennot[Komento.EROTUS].kumoa()
+        elif self._edellinen_komento[1] == "nollaus":
+            self._komennot[Komento.NOLLAUS].kumoa()
+        else:
+            pass
+        self._edellinen_komento = [0, ""]
 
 class Kayttoliittyma:
     def __init__(self, sovelluslogiikka, root):
         self._sovelluslogiikka = sovelluslogiikka
         self._root = root
+        self._edellinen_komento = [0, ""]
+
+        self._komennot = {
+            Komento.SUMMA: Summa(sovelluslogiikka, self._lue_syote, self._edellinen_komento),
+            Komento.EROTUS: Erotus(sovelluslogiikka, self._lue_syote, self._edellinen_komento),
+            Komento.NOLLAUS: Nollaus(sovelluslogiikka, self._lue_syote, self._edellinen_komento),
+            Komento.KUMOA: Kumoa(sovelluslogiikka, self._lue_syote, self._edellinen_komento)
+        }
 
     def kaynnista(self):
         self._arvo_var = StringVar()
@@ -54,23 +123,12 @@ class Kayttoliittyma:
         self._nollaus_painike.grid(row=2, column=2)
         self._kumoa_painike.grid(row=2, column=3)
 
+    def _lue_syote(self):
+        return self._syote_kentta.get()
+
     def _suorita_komento(self, komento):
-        arvo = 0
-
-        try:
-            arvo = int(self._syote_kentta.get())
-        except Exception:
-            pass
-
-        if komento == Komento.SUMMA:
-            self._sovelluslogiikka.plus(arvo)
-        elif komento == Komento.EROTUS:
-            self._sovelluslogiikka.miinus(arvo)
-        elif komento == Komento.NOLLAUS:
-            self._sovelluslogiikka.nollaa()
-        elif komento == Komento.KUMOA:
-            pass
-
+        komento_olio = self._komennot[komento]
+        komento_olio.suorita()
         self._kumoa_painike["state"] = constants.NORMAL
 
         if self._sovelluslogiikka.arvo() == 0:
